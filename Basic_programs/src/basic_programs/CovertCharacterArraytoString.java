abe72c4173b55896b077f75b50db14cedddbd5ba
thadojuvijay <vijaykumar6566@gmail.com>
2017-02-16 16:33:27 +0530

    commited to basic java programs

---
+++ Basic_programs/src/basic_programs/CovertCharacterArraytoString.java
@@ -0,0 +1,14 @@
+package basic_programs;
+
+public class CovertCharacterArraytoString {
+
+	public static void main(String[] args) {
+		char [] ch={'a','b','c','d','e'};
+		
+		String str=new String(ch);
+		
+		System.out.println(str);
+
+	}
+
+}
